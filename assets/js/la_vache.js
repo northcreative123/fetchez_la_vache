9bfbc2e947f9c9f184c6f1358b801799e3f546a9
Jeff Hill <jhill@northcreative.us>
2024-07-10 13:01:55 -0400

    menu updates

--- assets/js/la_vache.js
+++ assets/js/la_vache.js
@@ -39,7 +39,12 @@ const console_revisit_style = "color: pink; font-size: 18px; font-weight: bold;"
 const console_error_style = "color: red; font-size: 20px; font-weight: bold;"
 const console_pride_style = "padding: 10px 0 20px; font-weight: bold; font-size: 30px;color: white; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)"
 
-!is_prod && console.log('%c 🐄 Fetchez la Vache !', console_pride_style)
+if ( user_tag === "8989986" ) {
+    // window.open("https://www.youtube.com/watch?v=LLFhKaqnWwk", '_blank')
+    !is_prod && console.log('%c 🐄 Hello Jacob !!!', console_pride_style)
+} else {
+    !is_prod && console.log('%c 🐄 Fetchez la Vache !', console_pride_style)
+}
 
 /*
 !is_prod && console.log( '%c 🤓 console.log: ', console_key_style, 'message/data' )
